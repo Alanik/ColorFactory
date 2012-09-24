c1f4f8166cec5d9dde2d20f437e58abad7f05449
Alan Budzinski <thealanik@aim.com>
2012-09-24 02:18:18 +0200

    added canvas element

---
+++ ColorFactoryProject/ColorFactory/Scripts/functions.js
@@ -0,0 +1,7 @@
+﻿function DrawPattern() {
+    var can = document.getElementById('mapCanvas');
+    var ctx = can.getContext('2d');
+    
+    ctx.fillStyle = "rgb(188, 222, 178)";
+    ctx.fillRect(0, 0, 400, 400);
+}
